ca44fa21e599aa81e3545aca9de6802c58007810
Kyra Ezikeuzor <kyraezikeuzor@gmail.com>
2023-09-07 23:52:59 -0500

    Fixed vercel build error - removed fadeinsection

--- app/our-programs/page.js
+++ app/our-programs/page.js
@@ -2,13 +2,11 @@ import React from 'react';
 import Card from '../../components/Global/Card';
 import programData from '../../content/programs.json';
 import Banner from '../../components/Global/Banner';
-import FadeInSection from '../../components/Global/FadeInSection';
 import styles from './styles.module.css';
 
 function Programs() {
   return (
     <main className={styles.main}>
-      <FadeInSection>
         <header>
           <h2>Our Programs</h2>
           <p className={styles["caption"]}>Our programs offer an enriching and immersive experience that equips young minds with the knowledge and skills to thrive in the digital world.</p>
@@ -25,9 +23,6 @@ function Programs() {
           btnText="Sign Up"
         /> ))}
       </div>
-
-      
-      </FadeInSection>
     </main>
   )
 }
