27339f29e7fcae3b91de508ae81092e2dc4c0e9f
Kyra Ezikeuzor <kyraezikeuzor@gmail.com>
2023-10-03 19:36:31 -0500

    Next/font

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,13 @@
 import './globals.css'
 import Navbar from '../components/Global/Navbar';
 import Footer from '../components/Global/Footer';
-import head from 'next/head';
-import { Inter } from 'next/font/google';
-import Banner from '../components/Global/Banner';
+import {Inter} from 'next/font/google';
 
-import { config } from '@fortawesome/fontawesome-svg-core'
+import {config} from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
+config.autoAddCss = false;
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
   return (
@@ -18,6 +16,7 @@ export default function RootLayout({ children }) {
         <title>Compute the Future Foundation</title>
         <meta property="description" content="Compute the Future Foundation strives to educate young students in computer science and technology."/>
         <meta property="og:image" content="../images/cff-meta-image.png" />
+        
         <link href="https://api.fontshare.com/v2/css?f[]=zodiak@401&display=swap" rel="stylesheet"/>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
@@ -26,7 +25,6 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Navbar/>
           <main>{children}</main>
-        {/*<Banner text="Compute the Future Foundation strives to educate the young students in our areas in computer science and technology. Our goal is to impact the youth in our local communities by giving them the tools necessary to expand their interests and aspirations."/>*/}
         <Footer/>
       </body>
     </html>
