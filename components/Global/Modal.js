d29d8e685cbfa30ecdd345da1e4d4635a004bc94
Kyra Ezikeuzor <kyraezikeuzor@gmail.com>
2023-10-03 19:52:01 -0500

    Modal reverse

--- components/Global/Modal.js
+++ components/Global/Modal.js
@@ -1,3 +1,4 @@
+/*
 'use client'
 import React from 'react'
 import styles from './Modal.module.css'
@@ -9,20 +10,16 @@ export const Modal = ({setShowModal, src, title, text, tags}) => {
         <div onClick={()=>setShowModal(false)} className={styles.overlay}>
             <button onClick={()=>setShowModal(false)}>X</button>
             <div className={styles.modal}>
-                
                 <div className={styles["modal-container"]}>
-                
                     <img src={src}></img>
-                    
                     <div className={styles["modal-items"]}>
                         <h3>{title}</h3>
                         <p>{text}</p>
                         <Tags>{tags}</Tags>
                     </div>
-
                 </div>
-                
             </div>
         </div>
     );
 };
+*/
--- components/Global/Section.js
+++ components/Global/Section.js
@@ -1,16 +1,16 @@
 'use client'
-import React, {useState} from 'react'
-import {Button} from './Button'
+import React from 'react'
+//import {Button} from './Button'
 import styles from './Section.module.css'
 import Tags from './Tags'
-import Link from 'next/link'
-import {Modal} from './Modal'
+//import Link from 'next/link'
+//import {Modal} from './Modal'
 
 export default function Section({
-    src, alt, title, type, text, showBtn, modalText, modalTags, tags, flexDirection, flexOrder, imageSize
+    src, alt, title, type, text, /*showBtn, modalText, modalTags,*/ tags, flexDirection, flexOrder, imageSize
   }) {
 
-    const [showModal, setShowModal] = useState(false);
+   // const [showModal, setShowModal] = useState(false);
 
     const flexOrders = ['', 'row-reverse'];
     const checkFlexOrder = flexOrders.includes(flexOrder) ? flexOrder : flexOrders[0];
@@ -21,25 +21,20 @@ export default function Section({
     const imageSizes = ['large', 'medium'];
     const checkImageSize = imageSizes.includes(imageSize) ? imageSize : imageSizes[0];
 
-    const showBtns = [true, false];
-    const checkShowBtn = showBtns.includes(showBtn) ? showBtn : showBtns[0];
+    //const showBtns = [true, false];
+    //const checkShowBtn = showBtns.includes(showBtn) ? showBtn : showBtns[0];
 
   return (
     <section className={ `${styles["section"]} ${styles[checkImageSize]} ${styles[checkFlexDirection]} ${styles[checkFlexOrder]}`}>
         <img src={src} alt={alt}></img>
-
         <div className={styles["section-text-container"]}>
             <h4>{type}</h4>
             <h3>{title}</h3>
-          
             <p>{text}</p>
-
             <Tags>
               {tags}
             </Tags>
-
             {/*{checkShowBtn && <button onClick={() => setShowModal(true)}>Learn More</button>}*/}
-          
         </div>
         {/*{showModal && <Modal setShowModal={setShowModal} src={src} title={title} text={modalText} tags={modalTags}/>}*/}
     </section>
